c7a14a0cbfa9e93532982b94242a8e9d25082308
migas <miguel.migas.coelho@gmail.com>
2025-06-01 14:58:10 +0100

    Fix: catalog-service com endpoints corrigidos e SQLite funcional

--- admin-service/app.py
+++ admin-service/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route("/admin/videos")
 def test():
     try:
-        r = requests.get("http://catalog:5000/videos")
+        r = requests.get("http://ualflix_catalog:5000/videos")
         return Response(r.content, status=r.status_code, content_type='application/json')
     except Exception as e:
+        print("Erro ao contactar o catalog:", e)
         return Response(f'{{"error": "{str(e)}"}}', status=500, content_type='application/json')
 
 if __name__ == "__main__":
